d89d3fa3d02557e81e16e3f77115ec01199e39ef
Mohit Kumar <27889519+FailedCommit@users.noreply.github.com>
2022-12-07 14:18:04 +0530

    WIP: Kafka

---
+++ userservice/src/main/java/com/maatlabs/userservice/dtos/UserRegistrationRequest.java
@@ -0,0 +1,24 @@
+package com.maatlabs.userservice.dtos;
+
+import com.maatlabs.userservice.beans.QuizUser;
+import jakarta.validation.constraints.NotNull;
+
+public class UserRegistrationRequest {
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String email;
+    @NotNull
+    private String phone;
+
+    public QuizUser createUser() {
+        QuizUser user = new QuizUser();
+        user.setFirstName(this.firstName);
+        user.setLastName(this.lastName);
+        user.setEmail(this.email);
+        user.setPhone(this.phone);
+        return user;
+    }
+}
